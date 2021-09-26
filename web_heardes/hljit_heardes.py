d64b33a33575b1259d1e05c0f462c743457c506e
X <56810362+Day-Bright@users.noreply.github.com>
2021-09-26 15:31:31 +0800

    Add files via upload

---
+++ web_heardes/hljit_heardes.py
@@ -0,0 +1,47 @@
+hljit_heardes = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "Accept-Encoding": "gzip, deflate",
+    "Accept-Language": "en,zh-CN;q=0.9,zh;q=0.8",
+    "Connection": "keep-alive",
+    "Host": "jw.hljit.edu.cn",
+    "Pragma": "no-cache",
+    "Referer": "http://www.hljit.edu.cn/",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36"
+}
+
+checkcode_heardes = {
+    "Accept": "image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8",
+    "Accept-Encoding": "gzip, deflate",
+    "Accept-Language": "en,zh-CN;q=0.9,zh;q=0.8",
+    "Connection": "keep-alive",
+    "Host": "jw.hljit.edu.cn",
+    "Pragma": "no-cache",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36"
+}
+
+login_heardes = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "Accept-Encoding": "gzip, deflate",
+    "Accept-Language": "en,zh-CN;q=0.9,zh;q=0.8",
+    "Connection": "keep-alive",
+    "Content-Length": "822",
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Host": "jw.hljit.edu.cn",
+    "Origin": "null",
+    "Pragma": "no-cache",
+    "referer": "http://www.hljit.edu.cn/",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36"
+}
+
+main_heardes = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "Accept-Encoding": "gzip, deflate",
+    "Accept-Language": "en,zh-CN;q=0.9,zh;q=0.8",
+    "Connection": "keep-alive",
+    "Host": "jw.hljit.edu.cn",
+    "Pragma": "no-cache",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36"
+}
