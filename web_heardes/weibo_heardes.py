cbb3a770d2a6f6ffab6bf8f817925329ac06d50a
XYJ <56810362+Day-Bright@users.noreply.github.com>
2021-10-06 00:03:27 +0800

    Add files via upload

---
+++ web_heardes/weibo_heardes.py
@@ -0,0 +1,39 @@
+# -*- coding: UTF-8 -*-
+
+"""
+@Project ：website_login 
+@Product ：PyCharm
+@File    ：weibo_heardes.py
+@Date    ：2021/10/4 22:59 
+@Author  ：XYJ
+@Contact ：1520207872@qq.com
+"""
+
+getPubKey_heardes = {
+    "Accept": "*/*",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "en,zh-CN;q=0.9,zh;q=0.8",
+    "Connection": "keep-alive",
+    "Host": "login.sina.com.cn",
+    "Pragma": "no-cache",
+    "Referer": "https://weibo.com/",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36"
+}
+
+login_heardes = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "en,zh-CN;q=0.9,zh;q=0.8",
+    "Connection": "keep-alive",
+    "Content-Length": "734",
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Host": "login.sina.com.cn",
+    "Origin": "https://weibo.com",
+    "Pragma": "no-cache",
+    "Referer": "https://weibo.com/",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36"
+}
+
+if __name__ == '__main__':
+    pass
