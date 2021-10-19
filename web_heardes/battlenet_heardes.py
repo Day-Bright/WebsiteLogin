902ef7e3555b3355a2abf59c8ebf37915e2ac5cc
XYJ <1520207872@qq.com>
2021-10-20 00:21:24 +0800

    Add files via upload

---
+++ web_heardes/battlenet_heardes.py
@@ -0,0 +1,41 @@
+# -*- coding: UTF-8 -*-
+
+'''
+@Product ：VScode
+@File    ：battlenet_heardes.py
+@Date    ：2021/10/08 23:16:18
+@Author  ：XYJ
+@Contact ：1520207872@qq.com
+'''
+
+
+inf_heardes1 = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'en,zh-CN;q=0.9,zh;q=0.8',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive',
+    'Cookie': 'JSESSIONID=defd7515-678b-4e7e-8fab-f3fffac0fe0e.blade10_cn_b_login; bnet.extra=Af9MRCPlcPPA-VQMjpT3P5dn119wrgh5C7kPDfmo4EOOl9_5bcPxAWdt33jNmXTQm5mtGI0m146X48CvuQ4hGyMoUsp-SR4CZUZb3lX_7WFg; web.id=CN-a9bad26e-a91a-48d5-a50a-89f25506891f; _ga=GA1.3.1217771475.1626802826; _gid=GA1.3.1887017925.1633698918; _gcl_au=1.1.10086187.1633700003; _uetsid=4f222e10283c11ecb5a583fad163f382; _uetvid=4f229c60283c11eca30e1fc7f9422239; _scid=331f9ba0-a7ea-4020-be0f-5c9bc2805f36',
+    'Host': 'www.battlenet.com.cn',
+    'Pragma': 'no-cache',
+    'Referer': 'https://www.baidu.com/link?url=IAbC_kfntp7TNj3o5juWbNptpprmG-1b6OFB-ckmaUiP6whdjk4VA-ugtLGNoHsZ&wd=&eqid=f60f887f0006f027000000046160606e',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36'
+}
+
+
+inf_heardes2 = {
+    'Accept': 'application/json',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'zh-cn',
+    'Connection': 'keep-alive',
+    'Content-Length': '67',
+    'Content-Type': 'application/json',
+    # 'Cookie':' JSESSIONID=270bc95b-7fb4-4d60-bb36-79bb581293d3.blade14_cn_b_login; bnet.extra=AZ24FE5m2DAs8OH8GlCiEaqGTnosg57ehK2dKkC55yoj6WUw8Ojs1k1G7nib2mORY9bzNyqZFm4okqFewAMxvM4iRFiYOOP75qAUoRw4zaF6; web.id=CN-92455115-9846-49de-ab86-71276b8132cc; _ga=GA1.3.1775207221.1633792612; _gid=GA1.3.518392014.1633792612; _gat_UA-50249600-1=1; _gat_UA-50249600-81=1'
+    'Host': 'www.battlenet.com.cn',
+    'Origin': 'https://www.battlenet.com.cn',
+    'Pragma': 'no-cache',
+    'Referer': 'https://www.battlenet.com.cn/login/zh /',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36',
+    'X-Requested-With': 'XMLHttpRequest'
+}
